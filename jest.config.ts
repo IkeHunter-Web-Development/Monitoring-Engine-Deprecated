c70e8116b8b15908d2ab0df54c261c0d1ac9c465
Isaac Hunter <ikehunter5@gmail.com>
2023-11-26 21:38:37 -0500

    remove docs and index.ts from coverage

--- jest.config.ts
+++ jest.config.ts
@@ -37,7 +37,9 @@ const config: JestConfigWithTsJest = {
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
     "/node_modules/",
-    "/build/"
+    "/build/",
+    "docs/",
+    "index.ts"
   ],
 
   // Indicates which provider should be used to instrument code for coverage
